b0a348d11846cafd2f3629e5f1309a7cab1cbc47
aksel avetisyan <hello.dotoday@gmail.com>
2020-07-29 11:59:07 +0400

    Register page fixing

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,6 @@ import ToDoLists from "./dashboard/To-do-lists";
 import Login from "./auth/Login";
 import {auth} from "../services/Database";
 import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
-import SignUp from "./auth/Sign-up";
 
 export default class Main extends React.Component {
 
@@ -24,28 +23,25 @@ export default class Main extends React.Component {
 
     render() {
         return (
-                <Router>
-                    <Switch>
-                        <Route exact path="/dashboard">
-                            <div className="container">
-                                <ToDoLists/>
+            <Router>
+                <Switch>
+                    <Route exact path="/dashboard">
+                        <div className="container">
+                            <ToDoLists/>
+                        </div>
+                    </Route>
+                </Switch>
+                <Switch>
+                    <Route path="/auth">
+                        <div className="auth-container sign-in-container">
+                            <div className="container-bg">
+                                <Login/>
                             </div>
-                        </Route>
-                        <Route exact path="/auth">
-                            <div className="auth-container sign-in-container">
-                                <div className="container-bg">
-                                    <Login/>
-                                </div>
-                            </div>
-                        </Route>
-                        {/*<Route exact path="/auth/sign-up">*/}
-                        {/*    <div className="auth-container sign-up-container">*/}
-                        {/*        <SignUp/>*/}
-                        {/*    </div>*/}
-                        {/*</Route>*/}
-                    </Switch>
-                    <Redirect from="/" to={this.state.user ? '/dashboard' : '/auth'}/>
-                </Router>
+                        </div>
+                    </Route>
+                </Switch>
+                <Redirect to={this.state.user ? '/dashboard' : '/auth'}/>
+            </Router>
         )
     }
 }
